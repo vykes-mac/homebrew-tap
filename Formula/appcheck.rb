309dd90cc3b617ea3ee58080a9b31c59f37dd537
vykes <knrymcleish@gmail.com>
2026-01-30 20:31:56 -0500

    appcheck 0.1.3

--- Formula/appcheck.rb
+++ Formula/appcheck.rb
@@ -1,13 +1,13 @@
 class Appcheck < Formula
   desc "Local tooling to capture app walkthroughs and upload artifacts"
   homepage "https://github.com/vykes-mac/appcheck"
-  version "0.1.0"
+  version "0.1.3"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/vykes-mac/appcheck-releases/releases/download/v#{version}/appcheck-darwin-arm64.tgz"
-      sha256 "PLACEHOLDER_ARM64_SHA256"
+      sha256 "0aa95c527c8be5d12176cc107a40038aa10c6d299f33f9236a60a9b70711bf99"
 
       def install
         bin.install "appcheck"
@@ -17,7 +17,7 @@ class Appcheck < Formula
 
     on_intel do
       url "https://github.com/vykes-mac/appcheck-releases/releases/download/v#{version}/appcheck-darwin-x64.tgz"
-      sha256 "PLACEHOLDER_X64_SHA256"
+      sha256 "e26d0bff355addb26be3c2345ab589450bc9ccffb6f28dd078e34bfcf2f2a77e"
 
       def install
         bin.install "appcheck"
